886affe1720138277c6809c21c2d3e18e35a110f
Quentin <q.barthelemy@student.helmo.be>
2023-11-10 11:54:24 +0100

    Created message item

---
+++ src/lib/MessageItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let content: string;
+    export let time: string;
+    export let isCurrentUser = false;
+  </script>
+  
+  <div class="{isCurrentUser ? 'text-right' : 'text-left'}">
+    <div class="message-bubble {isCurrentUser ? 'bg-primary text-white' : 'bg-light'}">
+      <p class="mb-0">{content}</p>
+      <p class="mb-0 text-right">{time}</p>
+    </div>
+  </div>
+  
+  <style>
+    .message-bubble {
+      max-width: 70%;
+      padding: 10px;
+      border-radius: 10px;
+      margin: 5px;
+    }
+
+  </style>
