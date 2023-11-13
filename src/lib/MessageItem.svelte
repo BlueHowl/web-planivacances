6ab3aef5f93618e9ed3a53a2fde21757a4689c3c
Quentin <q.barthelemy@student.helmo.be>
2023-11-13 13:22:56 +0100

    updated message receive

--- src/lib/MessageItem.svelte
+++ src/lib/MessageItem.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+    export let sender: string;
     export let content: string;
     export let time: string;
     export let isCurrentUser = false;
   </script>
   
   <div class="{isCurrentUser ? 'text-right' : 'text-left'}">
+    <p>{sender}</p>
     <div class="message-bubble {isCurrentUser ? 'bg-primary text-white' : 'bg-light'}">
       <p class="mb-0">{content}</p>
       <p class="mb-0 text-right">{time}</p>
--- src/lib/Tchat.svelte
+++ src/lib/Tchat.svelte
@@ -8,11 +8,14 @@
     WebsocketEvent
   } from "websocket-ts";
     import InputMessage from "./InputMessage.svelte";
+    import MessageItem from "./MessageItem.svelte";
 
   const location = useLocation();
 
   let definedHoliday = false;
   let title: string;
+
+  let messages : any = [];
 
   if (location && $location.state) {
     definedHoliday = true;
@@ -28,22 +31,38 @@
 
   const OnMessageReceived = (i: Websocket, event: MessageEvent) => {
     console.log(`received message: ${event.data}`);
+    addMessage(event.data, false);
   };
 
-  tchatWS.addEventListener(WebsocketEvent.open, () => console.log("opened!"));
-  tchatWS.addEventListener(WebsocketEvent.close, () => console.log("closed!"));
+  function addMessage(message : any, isCurrentUser : boolean) {
+    message.isCurrentUser = isCurrentUser; //TODO fonctionne?
+    messages = [...messages, message];
+  }
+
+  tchatWS.addEventListener(WebsocketEvent.open, () => console.log("opened message websocket!"));
+  tchatWS.addEventListener(WebsocketEvent.close, () => console.log("closed message websocket!"));
   tchatWS.addEventListener(WebsocketEvent.message, OnMessageReceived);
 
 
   function sendMessage(event: CustomEvent) {
     tchatWS.send(JSON.stringify(event.detail.message));
+    addMessage({
+        sender : "username",
+        content : event.detail.message,
+        time : "time now"
+      }, 
+      false);
   }
 
 </script>
 
 {#if definedHoliday}
   <h1>Tchat - {title}</h1>
-  <section id="tchat" />
+  <section id="tchat">
+    {#each messages as message (message)}
+      <MessageItem sender={message.sender} content={message.content} time={message.time} isCurrentUser={false} />
+    {/each}
+  </section>
   <InputMessage on:send={sendMessage}></InputMessage>
 {:else}
   <h1>Impossible de charger le tchat pour ce voyage</h1>
