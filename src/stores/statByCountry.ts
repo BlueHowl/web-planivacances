0f921c8ced546fcd25d93ba4171a28021cbe0c08
Quentin <q.barthelemy@student.helmo.be>
2023-11-24 14:26:44 +0100

    removed persistence on stats by country store

--- src/stores/statByCountry.ts
+++ src/stores/statByCountry.ts
@@ -1,7 +1,4 @@
 import { writable, type Writable } from "svelte/store";
 import type { CountryUserMap } from "../model/CountryUserMap";
-import { createEncryptionStorage, createSessionStorage, GCMEncryption, persist } from "@macfja/svelte-persistent-store";
-import { STORE_ENCRYPTION_KEY } from "../utils/config";
 
-const storage = createEncryptionStorage(createSessionStorage(), new GCMEncryption(STORE_ENCRYPTION_KEY))
-export let userPerCountryStore: Writable<CountryUserMap | null> = persist(writable(null), storage, "userPerCountry") as Writable<CountryUserMap | null>
+export let userPerCountryStore: Writable<CountryUserMap | null> = writable(null)
