5914ef7f27add14a48abdaf21387df775fb6a96f
Quentin <q.barthelemy@student.helmo.be>
2023-12-17 18:11:24 +0100

    added API Endpoints Cypress tests done

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   },
 
   e2e: {
+    baseUrl: 'https://studapps.cg.helmo.be:5011/REST_CAO_BART/api',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/Connection.cy.ts
+++ cypress/e2e/Connection.cy.ts
@@ -2,28 +2,31 @@ import { mount } from 'cypress/svelte'
 
 describe('Connect to app', () => {
 
+  it('login with valid credentials', () => {
+    cy.request({
+      method: 'POST', 
+      url : 'auth/login', 
+      body : { 
+        mail : "test1234@web.com", 
+        password : "test1234" 
+      }
+    })
+    .its('body')
+    .should('not.be.null');
+  });
 
-  it('submit login infos', () => {
-    cy.visit("https://panoramix.cg.helmo.be/~d170051/PlaniVacances/");
-
-    cy.get('#username').type('q210043');
-    cy.get('#password').type('password');
-
-    cy.contains('Connexion').click()
-
-    cy.contains('Connexion').click()
-
-    // Type email
-    cy.get('input[name="email"]').type('test1234@web.com');
-  
-    // Type password
-    cy.get('input[name="password"]').type('test1234');
-
-    // Submit the form
-    cy.get('form').submit();
-    cy.contains('Connexion').click()
-
-    cy.url().should('equal', 'https://panoramix.cg.helmo.be/~d170051/PlaniVacances/');
+  it('login with invalid credentials', () => {
+    cy.request({
+      method: 'POST',
+      url: 'auth/login',
+      body: {
+        mail: 'testError@web.com',
+        password: 'testError',
+      },
+      failOnStatusCode: false,
+    })
+    .its('status')
+    .should('equal', 400)
   });
 
 });
